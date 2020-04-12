bb082550a7cfdcccb823907cf7a7f2b893e092d8
trunghien123 <44777619+trunghien123@users.noreply.github.com>
2020-04-12 12:44:55 +0700

    format giá

--- product-single.php
+++ product-single.php
@@ -89,13 +89,13 @@
 						<?php
 						if (!empty($row['KMSP'])) {
 							$price = $row['GIASP'] - ($row['GIASP'] * $row['KMSP']) / 100; ?>
-							<span class="mr-2 price-dc"><?php echo $row['GIASP']; ?></span>
+							<span class="mr-2 price-dc"><?php echo number_format($row['GIASP'],0,".",".").' Đ'; ?></span>
 						<?php
 						} else {
 							$price = $row['GIASP'];
 						}
 						?>
-						<span class="price-sale"><?php echo $price; ?></span></p>
+						<span class="price-sale"><?php echo number_format($price,0,".",".").' Đ'; ?></span></p>
 					<p><?php echo $row['MOTASP'] ?>
 					</p>
 					<div class="row mt-4">
